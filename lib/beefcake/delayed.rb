ca91ccd7b53ebf5bc90e3ef2101eb4e811513395
Kyle S. Goodwin <goodwink@gmail.com>
2012-09-04 09:28:56 -0400

    WIP

--- lib/beefcake/delayed.rb
+++ lib/beefcake/delayed.rb
@@ -2,24 +2,25 @@ module Beefcake
 
   class Delayed
     attr_accessor :klass, :bytes, :object
-    
+
     def initialize(klss, byts)
       @klass, @bytes = klss, byts
     end
-    
+
     def decode
       __object__
     end
-    
+
     def encode
       (@object) ? @object.encode : @bytes
     end
-    
+
     def __object__
+      puts @klass
       @object = Object.const_get(@klass).decode(@bytes.dup) unless @object
       @object
     end
-    
+
     alias :_inspect :inspect
     def inspect
       if @object
@@ -28,17 +29,17 @@ module Beefcake
         _inspect
       end
     end
-    
+
     def respond_to?(message)
       message = message.to_sym
       [:__object__, :inspect].include?(message) ||
         __object__.respond_to?(message)
     end
-    
+
     def method_missing(*a, &b)
       __object__.__send__(*a, &b)
     end
-    
+
   end
-  
+
 end
